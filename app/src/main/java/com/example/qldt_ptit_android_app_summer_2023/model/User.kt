f42235911ae305b2c89b9835432b5c303d5c079d
Trần Minh Châu <dluffy2332@gmail.com>
2023-07-14 16:16:40 +0700

    create User model

---
+++ app/src/main/java/com/example/qldt_ptit_android_app_summer_2023/model/User.kt
@@ -0,0 +1,22 @@
+package com.example.qldt_ptit_android_app_summer_2023.model
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.GET
+
+class User(var username: String,var password: String) {
+    @SerializedName("name")
+    var fullName: String? = null
+    lateinit var roles: String
+    @SerializedName("access_token")
+    lateinit var accessToken: String
+    @SerializedName("token_type")
+    lateinit var tokenType: String
+    fun isValidUsername(): Boolean{
+        return !username.equals("")
+    }
+
+    fun isValidPassword(): Boolean{
+        return !password.equals("")
+    }
+
+}
