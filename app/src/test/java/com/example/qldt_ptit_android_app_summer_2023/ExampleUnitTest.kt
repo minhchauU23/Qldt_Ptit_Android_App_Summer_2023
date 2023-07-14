b3b35890ed86a6a46c6f6892eed2e375ecd78f99
Trần Minh Châu <dluffy2332@gmail.com>
2023-07-14 14:57:23 +0700

    Create project

---
+++ app/src/test/java/com/example/qldt_ptit_android_app_summer_2023/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.qldt_ptit_android_app_summer_2023
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
