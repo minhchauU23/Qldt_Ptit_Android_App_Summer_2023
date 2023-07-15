535122947ba5b92c170dfbba8b36ccbfe8f6696f
Trần Minh Châu <dluffy2332@gmail.com>
2023-07-15 19:05:11 +0700

    create coroutines get user api

--- app/src/main/java/com/example/qldt_ptit_android_app_summer_2023/LoginActivity.kt
+++ app/src/main/java/com/example/qldt_ptit_android_app_summer_2023/LoginActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import com.example.qldt_ptit_android_app_summer_2023.api.QldtService
 import com.example.qldt_ptit_android_app_summer_2023.database.QldtHelper
 import com.example.qldt_ptit_android_app_summer_2023.model.User
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -35,47 +36,59 @@ class LoginActivity : AppCompatActivity() {
             var username = edtUsername.text.toString()
             var password = edtPassword.text.toString()
             var user = User(username, password)
-            if(user.isValidUsername() && user.isValidPassword())
-                loginWithInternet(user)
+            if(user.isValidUsername() && user.isValidPassword()){
+                if(isConnectInternet())
+                    callApi(user)
+                else loginWithoutInternet(user)
+                Log.d("user", user.accessToken)
+            }
             else Toast.makeText(applicationContext, "Invalid username or password", Toast.LENGTH_LONG)
         }
     }
 
-    fun loginWithoutInternet(user: User){
-        if(dbHelper.getUser(user)){
-
-        }
+    fun isConnectInternet() : Boolean{
+        return false
     }
 
-    fun loginWithInternet(user: User){
+    fun loginWithoutInternet(user: User){
+        dbHelper.getUser(user)
+    }
 
-        retrofit.login(user.username, user.password).enqueue(object : Callback<User>{
-            override fun onResponse(call: Call<User>, response: Response<User>) {
-                if(response.code() == 200){
-                    var body = response.body()
+    fun callApi(user: User){
+        GlobalScope.launch(Dispatchers.IO) {
+            var loginJob = launch {
+                var respone = retrofit.login(user.username, user.password)
+                Log.d("res code", respone.code().toString())
+                if(respone.code() == 200){
+                    var body = respone.body()
                     user.fullName = body?.fullName
                     user.roles = body?.roles!!
                     user.accessToken = body?.accessToken!!
                     user.tokenType = body?.tokenType!!
+                }
+                else{
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(applicationContext, "Invalid username or password", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            var insertUserJob = launch {
+                loginJob.join()
+                if(user.isInitialized()){
                     dbHelper.insertUser(user)
-//                    var checkSelect = User(user.username, user.password)
-//                    var isExist = dbHelper.getUser(checkSelect)
-//                    if(isExist){
-//                        Log.d("user", checkSelect.accessToken)
-//                    }
-//                    else Log.d("fail Select", "failt")'
-                }
-                else {
-                    Toast.makeText(applicationContext, response.message(), Toast.LENGTH_LONG)
+                    Log.d("insert", "inserting user with name = ${user.fullName}")
                 }
             }
 
-            override fun onFailure(call: Call<User>, t: Throwable) {
-
+            var getInforStudentJob = launch {
+                loginJob.join()
+                if(user.isInitialized()){
+                    
+                }
             }
-        })
 
 
+        }
     }
 
 }
