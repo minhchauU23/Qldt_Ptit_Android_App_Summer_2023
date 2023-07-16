df4d35744935ea7ca91a53689f5f0fbf02a99dba
Trần Minh Châu <dluffy2332@gmail.com>
2023-07-16 17:06:06 +0700

    create view pager in main

--- app/src/main/java/com/example/qldt_ptit_android_app_summer_2023/MainActivity.kt
+++ app/src/main/java/com/example/qldt_ptit_android_app_summer_2023/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.qldt_ptit_android_app_summer_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var bottomNavBar: BottomNavigationView
+    lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavBar = findViewById(R.id.bottom_nav_bar)
+        viewPager = findViewById(R.id.view_pager_main)
     }
 }
